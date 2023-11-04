fe206028ef15e9427df73a44e3ac1e942b1090d2
Jasmine Tai <jtai@jtai.ca>
2023-11-04 13:09:08 -0400

    Complete search API

--- food4kids-project/app/api/search/route.ts
+++ food4kids-project/app/api/search/route.ts
@@ -1,27 +1,57 @@
-import { nutritionix } from '@/api';
+import { spoonacular } from '@/spoonacular';
 import { SearchResult } from '@/types';
 import { NextResponse } from 'next/server';
 
 type ResponseData = {
-    /**
-     * results from Nutritionix.com branded food database (restaurant brands and
-     * grocery brands)
-     */
-    branded: SearchResult[];
-    /**
-     * results from a user's own food log (only if header x-user-jwt is
-     * provided, otherwise this result is null)
-     */
-    self: null;
-    /**
-     * results from Nutritionix.com common food database (curated list of
-     * averaged food items that are not brand-specific)
-     */
-    common: SearchResult[];
+    simpleFoods: SearchResult[];
+    products: SearchResult[];
 };
+
+interface SpoonacularFoodSearchResponse {
+    sorting: unknown;
+    filterMapping: unknown;
+    filterOptions: unknown;
+    activeFilterOptions: unknown;
+    query: string;
+    totalResults: number;
+    limit: number;
+    offset: number;
+    searchResults: {
+        name:
+            | 'Recipes'
+            | 'Products'
+            | 'Menu Items'
+            | 'Articles'
+            | 'Videos'
+            | 'Simple Foods';
+        type: 'products' | 'custom';
+        totalResults: number;
+        results: SearchResult[];
+    }[];
+    expires: number;
+    isStale: boolean;
+}
 
 export async function GET(req: Request): Promise<NextResponse<ResponseData>> {
     const query = new URL(req.url).searchParams.get('q')?.toString() ?? '';
-    const data = await nutritionix('GET', '/search/instant', { query });
-    return NextResponse.json(data as ResponseData);
+    const data = (await spoonacular('GET', '/food/search', {
+        query,
+        offset: '0',
+        number: '10',
+    })) as SpoonacularFoodSearchResponse;
+
+    const response: ResponseData = {
+        simpleFoods: [],
+        products: [],
+    };
+
+    for (const category of data.searchResults) {
+        if (category.name === 'Simple Foods') {
+            response.simpleFoods = category.results;
+        } else if (category.name === 'Products') {
+            response.products = category.results;
+        }
+    }
+
+    return NextResponse.json(response);
 }
--- food4kids-project/types.ts
+++ food4kids-project/types.ts
@@ -1,13 +1,10 @@
 /** Search result from Nutritionix database */
 export interface SearchResult {
-    food_name: string;
-    image: string | null;
-    serving_unit: string;
-    nix_brand_id: string;
-    brand_name_item_name: string;
-    serving_qty: number;
-    nf_calories: number;
-    brand_name: string;
-    brand_type: number;
-    nix_item_id: string;
+    id: number;
+    name: string;
+    image: string;
+    type: 'HTML' | 'YOUTUBE_VIDEO';
+    relevance: number;
+    content: unknown;
+    dataPoints: unknown[];
 }
